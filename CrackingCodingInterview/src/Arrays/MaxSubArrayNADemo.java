8be1f36ba3fb2aab1fc136f7576c2c1e76054d6a
nishanth prakash <nishanth.prakash@hpe.com>
2016-11-20 16:34:13 -0700

    More on Arrays

---
+++ CrackingCodingInterview/src/Arrays/MaxSubArrayNADemo.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+/**
+ * Non adjacent - all positive integers.
+ * 
+ * @author prakasni
+ *
+ */
+public class MaxSubArrayNADemo {
+	public static void main(String[] args) {
+		int[] input = { 4, 1, 1, 4, 2, 1 };
+		MaxSubArrayNADemo demo = new MaxSubArrayNADemo();
+		System.out.println(demo.nonAdjacentSum(input));
+	}
+
+	public int nonAdjacentSum(int[] input) {
+		int exclusive = 0;
+		int inclusive = input[0];
+		for (int i = 1; i < input.length; i++) {
+			int temp = inclusive;
+			inclusive = Math.max(inclusive, exclusive + input[i]);
+			exclusive = temp;
+		}
+		return inclusive;
+	}
+}
