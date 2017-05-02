85e6742beb25c85ae346857fb4abb6994d4270f3
Nishanth Prakash <nishanth.prakash@hpe.com>
2017-05-01 23:49:24 -0600

    Adding generic type clases in java

---
+++ PopularInterviewQuestions/Threads/src/JavaConcepts/GenericMethodDemo.java
@@ -0,0 +1,26 @@
+package JavaConcepts;
+
+/**
+ * 
+ * @author prakasni 
+ * Usually the convention followed is T or E not a A.
+ */
+public class GenericMethodDemo {
+	public <A> A printArray(A[] inputArray) {
+		A sum = null;
+		for (A input : inputArray) {
+			System.out.println(input);
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+
+		Integer i[] = { 1, 2, 3, 4 };
+		String[] data = { "n", "i", "s" };
+
+		GenericMethodDemo d = new GenericMethodDemo();
+		d.printArray(i);
+		d.printArray(data);
+	}
+}
