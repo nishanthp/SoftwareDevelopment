913d76c40bfe5e41fbbffec9dce5dbc969147ae3
Nishanth Prakash <nishanth.prakash@hpe.com>
2017-06-17 16:30:27 -0600

    More TQ

--- PopularInterviewQuestions/CodingQuestions/src/newSet/ExcelColumnDemo.java
+++ PopularInterviewQuestions/CodingQuestions/src/newSet/ExcelColumnDemo.java
@@ -1,8 +1,49 @@
 package newSet;
 
-public class ExcelColumnDemo {
-	public static void main(String []args) {
+import java.util.ArrayList;
+import java.util.Stack;
 
+public class ExcelColumnDemo {
+	public static void main(String[] args) {
+		int number = 705;
+		ExcelColumnDemo d = new ExcelColumnDemo();
+		String name = d.findColmnName(number);
+		System.out.println(name);
+	}
+
+	private String findColmnName(int number) {
+		Stack<Character> charArray = new Stack<>();
+		String previous = null;
+		if (number > 26) {
+			int remainder = number % 26;
+			charArray.add(convertNumberToAscii(remainder));
+			int quo = number / 26;
+			if (quo > 26) {
+				previous = findColmnName(quo);
+			} else {
+				charArray.add(convertNumberToAscii(quo));
+			}
+		} else {
+			charArray.add(convertNumberToAscii(number));
+		}
+		return convertCharArrayToString(previous, charArray);
+
+	}
+
+	private String convertCharArrayToString(String previous, Stack<Character> charStack) {
+		StringBuffer buffer = new StringBuffer();
+		while (!charStack.isEmpty()) {
+			buffer.append(charStack.pop());
+		}
+		if (previous != null)
+			return previous + buffer.toString();
+		else
+			return buffer.toString();
+	}
+
+	private char convertNumberToAscii(int remainder) {
+		int BASE = 65;
+		return (char) (BASE + remainder - 1);
 	}
 
 }
