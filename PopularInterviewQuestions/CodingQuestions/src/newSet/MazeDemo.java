5419366101bffcf2c1a8fbceac1cd3a5a58962fe
Nishanth Prakash <nishanth.prakash@hpe.com>
2017-06-28 22:10:42 -0600

    TQ

---
+++ PopularInterviewQuestions/CodingQuestions/src/newSet/MazeDemo.java
@@ -0,0 +1,22 @@
+package newSet;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class MazeDemo {
+	public static void main(String[] args) {
+		int N = 4;
+		LinkedList<Integer> places = new LinkedList<>();
+		int i = N - 1;
+		int j = N - 1;
+		ratMaze(i, j, places);
+	}
+
+	private static void ratMaze(int n, int j, LinkedList<Integer> places) {
+		int i=0;
+		if (i <= 0) {
+
+		}
+	}
+
+}
