eec62827334b9965d7c50cbb1e45ef60a950ad97
Nishanth Prakash <nishanth.prakash@hpe.com>
2017-07-06 00:14:44 -0600

    Pramp

---
+++ PopularInterviewQuestions/CodingQuestions/src/newSet/DroneHeightDemo.java
@@ -0,0 +1,13 @@
+package newSet;
+
+public class DroneHeightDemo {
+	public static void main(String[] args) {
+		int[][] input = new int[5][3];
+		input[0] = {1,2,3};
+		input[1] = {};
+		input[2] = {};
+		input[3] = {};
+		input[4] = {};
+
+	}
+}
