370257981729191f059ed632fc89ba998b322f67
Nishanth Prakash <nishanth.prakash@gmail.com>
2017-07-11 01:28:39 -0600

    Modified

--- PopularInterviewQuestions/CrackingCodingInterview/src/Arrays/FindPalindromeInPlaceDemo.java
+++ PopularInterviewQuestions/CrackingCodingInterview/src/Arrays/FindPalindromeInPlaceDemo.java
@@ -1,6 +1,6 @@
 package Arrays;
 
-public class FindPalindromeConstantSizeDemo {
+public class FindPalindromeInPlaceDemo {
 	public static void main(String[] args) {
 		String input = "abccba";
 		findWhetherPalindrome(input);
