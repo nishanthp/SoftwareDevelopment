91375daff30eb63a1122b6af0d3139891b1918ec
Nishanth Prakash <nishanth.prakash@hpe.com>
2017-07-29 21:24:37 -0600

    Pramp

--- PopularInterviewQuestions/CodingQuestions/src/newSet/PancakeSortDemo.java
+++ PopularInterviewQuestions/CodingQuestions/src/newSet/PancakeSortDemo.java
@@ -1,7 +1,28 @@
 package newSet;
 
 public class PancakeSortDemo {
-public static void main(String []args){
-	
-}
+	public static void main(String[] args) {
+		int[] input = { 1, 5, 4, 3, 2 };
+		PancakeSortDemo d = new PancakeSortDemo();
+		for (int result : d.pancakeSort(input)) {
+			System.out.println(result);
+		}
+	}
+
+	public int[] pancakeSort(int[] input) {
+
+	}
+
+	public int[] flip(int[] input, int k) {
+		int rightIndex = k;
+		int leftindex = 0;
+		while (rightIndex > leftindex) {
+			int temp = input[leftindex];
+			input[leftindex] = input[rightIndex];
+			input[rightIndex] = temp;
+			leftindex++;
+			rightIndex--;
+		}
+		return input;
+	}
 }
