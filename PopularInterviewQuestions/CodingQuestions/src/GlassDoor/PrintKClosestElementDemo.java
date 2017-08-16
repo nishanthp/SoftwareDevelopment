b4e3fc9c9dbe54c3c8e963be3f8c12d3302ad1f8
Nishanth Prakash <nishanth.prakash@hpe.com>
2017-08-15 22:29:34 -0600

    more tq

---
+++ PopularInterviewQuestions/CodingQuestions/src/GlassDoor/PrintKClosestElementDemo.java
@@ -0,0 +1,67 @@
+package GlassDoor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PrintKClosestElementDemo {
+	public static void main(String[] args) {
+		int[] input = { 12, 16, 22, 30, 35, 39, 42, 45, 48, 50, 53, 55, 56 };
+		int num = 35;
+		int k = 4;
+		for (Integer i : findKPoints(input, num, k)) {
+			System.out.println(i);
+		}
+	}
+
+	private static ArrayList<Integer> findKPoints(int[] input, int num, int k) {
+		ArrayList<Integer> list = new ArrayList<>();
+		int index = searchElement(input, 0, input.length - 1, num);
+		System.out.println(index);
+		if (index != -1) {
+			int left = index - 1;
+			int right = index + 1;
+			while (left >= 0 && right <= input.length && list.size() < k) {
+				if (input[index] - input[left] < input[right] - input[index]) {
+					list.add(input[left]);
+					left--;
+				} else {
+					list.add(input[right]);
+					right++;
+				}
+			}
+
+			while (list.size() < k && right <= input.length) {
+				list.add(input[right]);
+				right++;
+
+			}
+
+			while (list.size() < k && left >= 0) {
+				list.add(input[left]);
+				left--;
+
+			}
+
+		} else {
+			return null;
+		}
+		return list;
+	}
+
+	private static int searchElement(int[] input, int left, int right, int num) {
+		int mid = (left + right) / 2;
+		if (right < left || left < 0 || right > input.length - 1) {
+			return -1;
+		}
+		if (input[mid] == num) {
+			return mid;
+		}
+
+		if (input[mid] > num) {
+			return searchElement(input, left, mid - 1, num);
+		} else {
+			return searchElement(input, mid + 1, right, num);
+		}
+
+	}
+}
---
+++ PopularInterviewQuestions/CodingQuestions/src/GlassDoor/ReplaceAllStringDemo.java
@@ -0,0 +1,27 @@
+package GlassDoor;
+
+public class ReplaceAllStringDemo {
+	public static void main(String[] args) {
+		String input = "thisishowitworks";
+		String pattern = "works";
+		String replacement = "shouldworks";
+		ReplaceAllStringDemo d = new ReplaceAllStringDemo();
+		System.out.println(d.replaceAll(input, pattern, replacement));
+	}
+
+	private String replaceAll(String input, String pattern, String replacement) {
+		StringBuffer buffer = new StringBuffer();
+		int startIndex = 0;
+		while (startIndex < input.length()) {
+			int patterIndex = input.indexOf(pattern, startIndex);
+			if (patterIndex > 0) {
+				buffer.append(input.substring(startIndex, patterIndex));
+				buffer.append(replacement);
+				startIndex = patterIndex + pattern.length();
+			}
+		}
+		// last part of the string.
+			buffer.append(input.substring(startIndex));
+		return buffer.toString();
+	}
+}
