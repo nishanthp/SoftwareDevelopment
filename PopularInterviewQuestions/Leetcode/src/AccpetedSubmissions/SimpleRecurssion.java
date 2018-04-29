ab871ffb833022679a0072e3f99efde286fb9447
Nishanth Prakash <nishanth.prakash@hpe.com>
2018-04-29 15:14:26 -0700

    Leetcode questions

---
+++ PopularInterviewQuestions/Leetcode/src/AccpetedSubmissions/SimpleRecurssion.java
@@ -0,0 +1,32 @@
+package AccpetedSubmissions;
+
+public class SimpleRecurssion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		System.out.println(recurssionWithVar(i));
+		int[] array = new int[10];
+		recurssionWithArray(array, i);
+		for (int each : array) {
+			System.out.println(each);
+		}
+	}
+
+	public static int recurssionWithVar(int i) {
+		if (i >= 10)
+			return i;
+		i += 1;
+		i = recurssionWithVar(i);
+		return i;
+	}
+
+	public static int[] recurssionWithArray(int[] array, int i) {
+		if (i >= 10)
+			return array;
+		array[i] = i;
+		i += 1;
+		recurssionWithArray(array, i);
+		return array;
+	}
+}
---
+++ PopularInterviewQuestions/Leetcode/src/AccpetedSubmissions/ValidateBST.java
@@ -0,0 +1,23 @@
+package AccpetedSubmissions;
+
+public class ValidateBST {
+
+	public static void main(String[] args) {
+		// Ran on leetcode.
+
+	}
+
+	public boolean isValidBST(TreeNode root) {
+		return isValid(root, 0, 0);
+	}
+
+	public boolean isValid(TreeNode root, int max, int min) {
+		if (root == null)
+			return true;
+		if (min != 0 && root.val <= min || max != 0 && root.val >= max)
+			return false;
+		if (root.left != null && root.left.val >= root.val || root.right != null && root.right.val <= root.val)
+			return false;
+		return isValid(root.left, root.val, min) && isValid(root.right, max, root.val);
+	}
+}
