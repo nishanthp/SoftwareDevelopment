d19183c867ff099a1e01fed1d07e8f3dbaf225aa
Nishanth Prakash <nishanth.prakash@hpe.com>
2018-06-09 21:46:50 -0700

    More leetcode

---
+++ PopularInterviewQuestions/Leetcode/src/AccpetedSubmissions/MinimumSizeSubArray.java
@@ -0,0 +1,29 @@
+package AccpetedSubmissions;
+
+public class MinimumSizeSubArray {
+
+	public static void main(String[] args) {
+		// Ran on leetcode.
+
+	}
+
+	/**
+	 * s = 7, nums = [2,3,4,2,1,3]
+	 *
+	 * 
+	 */
+	public int minSubArrayLen(int s, int[] nums) {
+		int minSize = Integer.MAX_VALUE;
+		int left = 0;
+		int sum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			while (sum >= s) {
+				minSize = Math.min(minSize, (i + 1) - left);
+				sum -= nums[left++];
+			}
+		}
+		return minSize == Integer.MAX_VALUE ? 0 : minSize;
+	}
+
+}
