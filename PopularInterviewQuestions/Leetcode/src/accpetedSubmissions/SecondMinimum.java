64bbb7ca00342648e92e3d32004caa0ddea3733a
Nishanth Prakash <nishanth.prakash@hpe.com>
2018-07-20 19:02:55 -0700

    More leet

---
+++ PopularInterviewQuestions/Leetcode/src/accpetedSubmissions/SecondMinimum.java
@@ -0,0 +1,17 @@
+package accpetedSubmissions;
+
+public class SecondMinimum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int findSecondMinimumValue(TreeNode root) {
+        if(root == null || root.left == null || root.right == null) return -1;
+        int left = root.val == root.left.val ? findSecondMinimumValue(root.left) : root.left.val;
+        int right = root.val == root.right.val ? findSecondMinimumValue(root.right) : root.right.val;
+        return left == -1 || right == -1 ? Math.max(left, right) : Math.min(left, right);
+            
+    }
+}
