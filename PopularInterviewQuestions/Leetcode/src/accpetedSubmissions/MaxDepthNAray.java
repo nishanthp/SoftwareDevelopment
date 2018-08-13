4ab2afa3534c96b97f416d9aa0112bce533ad086
nishanthp <nishanth.prakash@gmail.com>
2018-08-13 01:04:07 -0700

    More lt

---
+++ PopularInterviewQuestions/Leetcode/src/accpetedSubmissions/MaxDepthNAray.java
@@ -0,0 +1,26 @@
+package accpetedSubmissions;
+
+public class MaxDepthNAray {
+
+	public static void main(String[] args) {
+		// Ran on leetcode.
+
+	}
+
+	int maxDepth = 0;
+    public int maxDepth(Node root) {
+        if(root == null) return 0;
+        //Stack<Node> stack = new Stack<Node>();
+        //stack.push(root);     
+        //while(!stack.isEmpty()){
+            //Node c = stack.pop();
+            int depth = 0;
+            for(Node child : root.children){
+                //stack.push(child);
+                depth = Math.max(depth, maxDepth(child));
+                maxDepth = Math.max(depth, maxDepth);
+            }
+        //}
+        return 1+depth;
+    }
+}
