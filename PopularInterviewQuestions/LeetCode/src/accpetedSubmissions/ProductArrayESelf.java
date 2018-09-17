6df814c16d44cb5abb6e7f6e3e40fda27ea2a048
nishanthp <nishanth.prakash@gmail.com>
2018-09-16 21:08:14 -0700

    More leetcode

---
+++ PopularInterviewQuestions/LeetCode/src/accpetedSubmissions/ProductArrayESelf.java
@@ -0,0 +1,25 @@
+package accpetedSubmissions;
+
+public class ProductArrayESelf {
+
+	public static void main(String[] args) {
+		// Ran on leetcode.
+
+	}
+	
+	// done in constant space (expect the resultant array itself)
+	 public int[] productExceptSelf(int[] nums) {
+	        int[] result = new int[nums.length];
+	        result[0] = 1;
+	        for(int i=1;i<nums.length;i++){
+	            result[i] = result[i-1] * nums[i-1]; 
+	        }
+	        int right = 1;
+	        for(int i=nums.length-1;i>=0;i--){
+	            result[i] =  result[i] * right; 
+	            right = right * nums[i];
+	        }
+	        return result;
+	    }
+
+}
