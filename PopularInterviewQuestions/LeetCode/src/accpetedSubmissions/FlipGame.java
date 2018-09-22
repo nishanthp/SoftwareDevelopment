bb0160b9c31b402cd76bd473feb6c66ed4b99e0f
nishanthp <nishanth.prakash@gmail.com>
2018-09-22 15:26:27 -0700

    More lt

---
+++ PopularInterviewQuestions/LeetCode/src/accpetedSubmissions/FlipGame.java
@@ -0,0 +1,25 @@
+package accpetedSubmissions;
+
+public class FlipGame {
+
+	public static void main(String[] args) {
+		// Ran on leetcode.
+
+	}
+	
+	// Same as recursive backtracking. 
+	public boolean canWin(String s) {
+        if(s == null || s.length() < 2) return false; 
+        
+        for(int i=0;i<s.length(); i++){
+            if(s.startsWith("++", i)){
+                String temp = s.substring(0, i) + "--" + s.substring(i+2);
+                if(!canWin(temp)) return true;
+                
+            }
+        }
+        
+        return false;
+    }
+
+}
