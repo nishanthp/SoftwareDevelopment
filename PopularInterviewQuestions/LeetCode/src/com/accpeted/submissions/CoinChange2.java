db460200204cd003b3d9a3c51a7d5ef511dae514
nishanthp <nishanth.prakash@gmail.com>
2019-02-24 19:40:07 -0800

    more leetcode

---
+++ PopularInterviewQuestions/LeetCode/src/com/accpeted/submissions/CoinChange2.java
@@ -0,0 +1,33 @@
+package com.accpeted.submissions;
+
+public class CoinChange2 {
+
+	public static void main(String[] args) {
+		// Ran on leetcode.
+
+	}
+	
+	// number of combinations. 
+	// if j >= coins[i-1]
+	// m[i][j] = m[i-1][j] || m[i][j-coins[i-1]]
+	// else m[i][j] = m[i-1][j].
+	 public int change(int amount, int[] coins) {
+	        int[][] matrix = new int[coins.length+1][amount+1];
+	        
+	        for(int i=0;i<matrix.length;i++){
+	            matrix[i][0] = 1;
+	        }
+	        for(int i=1;i<matrix.length;i++){
+	            for(int j=1;j<matrix[0].length;j++){
+	                if(j>=coins[i-1]) {
+	                    matrix[i][j] = matrix[i][j-coins[i-1]] + matrix[i-1][j];
+	                    System.out.println(matrix[i][j]);
+	                } else {
+	                    matrix[i][j] = matrix[i-1][j];
+	                }
+	            }
+	        }
+	        return matrix[coins.length][amount];
+	    }
+
+}
