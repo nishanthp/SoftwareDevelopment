2a27a44fa64f5d781ddb4e1b24a139e596a9e6ba
nishanthp <nishanth.prakash@gmail.com>
2019-03-10 18:44:45 -0700

    union find

---
+++ PopularInterviewQuestions/LeetCode/src/com/accpeted/submissions/RedundantConnection.java
@@ -0,0 +1,30 @@
+package com.accpeted.submissions;
+
+import java.util.Arrays;
+
+public class RedundantConnection {
+
+	public static void main(String[] args) {
+		// Ran on leetcode.
+
+	}
+	
+	// follow the union find template. Find the cycle in a graph.
+	public int[] findRedundantConnection(int[][] edges) {
+        int [] parent = new int[edges.length+1];
+        Arrays.fill(parent, -1);
+        for(int[] edge : edges) {
+            int x = findParent(parent, edge[0]);
+            int y = findParent(parent, edge[1]);
+            if(x == y) return edge;
+            parent[x] = y;
+        }
+        return new int[2];
+    }
+    
+    public int findParent(int[] parent, int i){
+        if(parent[i] == -1) return i;
+        return findParent(parent, parent[i]);
+    }
+
+}
