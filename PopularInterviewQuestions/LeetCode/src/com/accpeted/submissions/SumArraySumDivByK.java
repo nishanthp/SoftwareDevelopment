1fa93b031108feca2a809556b2b6644d132a93e0
nishanthp <nishanth.prakash@gmail.com>
2019-09-23 20:17:04 -0700

    More lt

---
+++ PopularInterviewQuestions/LeetCode/src/com/accpeted/submissions/SumArraySumDivByK.java
@@ -0,0 +1,26 @@
+package com.accpeted.submissions;
+
+import java.util.HashMap;
+
+public class SumArraySumDivByK {
+
+	public static void main(String[] args) {
+		// Ran on leetcode.
+	}
+	   public int subarraysDivByK(int[] A, int K) {
+	        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+	        map.put(0,1);
+	        int runningSum = 0;
+	        int count = 0 ;
+	        for(int i=0;i<A.length;i++){
+	            runningSum += A[i];
+	            int remainder = runningSum % K;
+	            if(remainder < 0) remainder +=K;
+	            if(map.containsKey(remainder)) {
+	                count += map.get(remainder);      
+	            }
+	             map.put(remainder, map.getOrDefault(remainder,0)+1);  
+	        }
+	         return count;
+	    }
+}
